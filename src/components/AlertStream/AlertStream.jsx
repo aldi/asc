2e1683c31ca3f8a3fb1600a1802f3b11f7f47773
aldi <aldiduzha@gmail.com>
2021-01-16 17:52:35 +0200

    Update AlertStream Component

--- src/components/AlertStream/AlertStream.jsx
+++ src/components/AlertStream/AlertStream.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from 'react';
+import Card from '../Card';
 
 // Data available
 // ------------------
@@ -15,35 +15,53 @@ import styled from "styled-components";
 // description: chance.paragraph({ sentences: 2 }),
 // time: new Date()
 
-const AlertItems = styled.ul``;
-const AlertItem = styled.li`
-  text-align: left;
-`;
+const AlertStream = ({ data }) => {
+  const [highSeverityItems, setHighSeverityItems] = useState([]);
+  const [mediumSeverityItems, setMediumSeverityItems] = useState([]);
+  const [lowSeverityItems, setLowSeverityItems] = useState([]);
 
-const AlertStream = ({ data }) => (
-  <AlertItems>
-    {data.map(
-      ({
-        key,
-        title,
-        severity,
-        type,
-        isPrediction,
-        predictionConfidence,
-        description,
-        time
-      }) => (
-        <AlertItem key={key}>
-          <p>{title}</p>
-          <p>{severity}</p>
-          <p>{type}</p>
-          <p>{isPrediction ? "YES" : "NO"}</p>
-          <p>{description}</p>
-          <p>{time.toString()}</p>
-        </AlertItem>
-      )
-    )}
-  </AlertItems>
-);
+  useEffect(() => {
+    if (data) {
+      setHighSeverityItems(data.filter((item) => item.severity === 1));
+      setMediumSeverityItems(data.filter((item) => item.severity === 2 || item.severity === 3));
+      setLowSeverityItems(data.filter((item) => item.severity === 4 || item.severity === 5));
+    }
+  }, [data]);
+
+  return (
+    <div className="columns pt-4">
+      <div className="column alerts-wrapper is-one-third">
+        <p className="title is-4 my-1 has-text-weight-bold has-text-centered">
+          High Severity Alerts
+        </p>
+        <div className="alerts-column">
+          {highSeverityItems.map((item) => (
+            <Card item={item} key={item.key} />
+          ))}
+        </div>
+      </div>
+      <div className="column alerts-wrapper is-one-third">
+        <p className="title is-4 my-1 has-text-weight-bold has-text-centered">
+          Medium Severity Alerts
+        </p>
+        <div className="alerts-column">
+          {mediumSeverityItems.map((item) => (
+            <Card item={item} key={item.key} />
+          ))}
+        </div>
+      </div>
+      <div className="column alerts-wrapper is-one-third">
+        <p className="title is-4 my-1 has-text-weight-bold has-text-centered">
+          Low Severity Alerts
+        </p>
+        <div className="alerts-column">
+          {lowSeverityItems.map((item) => (
+            <Card item={item} key={item.key} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AlertStream;
